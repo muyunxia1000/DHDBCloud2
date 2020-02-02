9976833c8ef60ffda40d903343d1c11de9e235ee
muyunxia <muyunxia@>
2020-02-02 18:28:06 +0800

    添加项目文件。

---
+++ DHDBCloud/DHDBCloud.h
@@ -0,0 +1,27 @@
+// DHDBCloud.h : DHDBCloud DLL 的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CDHDBCloudApp
+// 有关此类实现的信息，请参阅 DHDBCloud.cpp
+//
+
+class CDHDBCloudApp : public CWinApp
+{
+public:
+	CDHDBCloudApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+	DECLARE_MESSAGE_MAP()
+};
